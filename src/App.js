95ad99e6deeec80d0449ccbe30515a4dc1b283c1
azzasouei <azzasouei@gmail.com>
2023-04-28 16:18:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button , Form, FormGroup,Label, Input } from 'reactstrap';
+import { FacebookLoginButton } from 'react-social-login-buttons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="Login-form">
+      <h2> Form checkpoint </h2>  
+      <h3 className='text-center'> Welcome </h3>
+      <FormGroup>
+        <Label> Email</Label>
+        <Input type='email' placeholder='email' />
+      </FormGroup>
+      <FormGroup>
+        <Label> password</Label>
+        <Input type='password' placeholder='password' />
+      </FormGroup>
+      <Button className="btn-lg btn-dark btn-block" > 
+      Log in 
+      </Button>
+      <div className="text-center pt-3">Or continue with your facebook account 
+      </div>
+      <FacebookLoginButton className='mb-3 mt-3'/>
+      <div className='text-center'>
+        <a href="/sign-up">Sign up </a> 
+        <span className='p-2'>|</span> 
+        <a href="/forgot-password">Forgot password </a> 
+      </div>
+    </Form>
   );
 }
 
